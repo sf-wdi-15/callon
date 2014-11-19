0791b2998097e148c3dbf5bcb2991760f864b2b1
Michael Desa <mjdesa@gmail.com>
2014-11-18 20:17:27 -0800

    Initial Version

---
+++ callon.js
@@ -0,0 +1,9 @@
+#! /usr/local/bin/node
+var students = ["Laurie Alaoui", "Josh Andrews", "Sam Cabral", "Nalin Chopra", "Karen Chu", "Angelo Cisneros",
+  "Alexandra Connell", "Joe Dale", "Mark Daniels", "Alexander Fulsome", "Bryan Hernandez", "Alex Lara",
+  "Stephen Lathbury", "Allison Marino", "Ky Meyer-Choi", "Jeff Morris", "Emannuel Nevarez", "Andrew Pohl",
+  "Ajesh Shah", "Sandra Stark"];
+
+var index = Math.floor(Math.random() * 20);
+
+console.log(students[index]);
